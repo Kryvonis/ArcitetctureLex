9ab0296ff07c1d255e17448278df058d46a39278
Kryvonis <kryvonis.artem@gmail.com>
2016-05-13 14:22:49 +0300

    init commit

--- lex.py
+++ lex.py
@@ -305,7 +305,8 @@
 
 	import modulel
 	modulel.a ---> 1
-======Class=====================================================================================================================================================================================:
+@04/08
+======Class (part 1)============================================================================================================================================================================:
 	1. 3 Механізми
 	2. 5 принципів SOLID (Роберт Мартін "Гнучка розробка ля ля ля")
 	3. >20 патернів 
@@ -363,17 +364,324 @@
 		def test_f(obj,b):
 			b.x = 42
 			self.assertEqual(f(),42)
+@04/15
+======Class (part 2)============================================================================================================================================================================:
+	isinstance(x,int) враховує іерархію успадкування
+	це краще ніж if type(x) == int
+
+	import numbers - це модуль визначає всі базові типи
+
+	numbers.Number - це всі можливі числові типи
+	isinstance(x,numbers.Number) - найкращий варіант
+
+	class ClassName(object):
+		"""docstring for ClassName"""
+		def __init__(self, arg):
+			self.arg = arg - це публічна змінна
+			self._arg = arg - це "приватна" змінна
+			self.__arg = arg - це приватна змінна. Не буде доступна за межами нашого класу (майже)
+
+	c = ClassName(2)
+	vars(p)
+
+	{'arg':2,'_arg':2,'_ClassName__arg':2}  для secret змінних пайтон застсовує спотворення імен.
+	Це спеціальні імена які ми можемо використовувати для класів які успадкованні один від одного
+
+	class A(object):
+		"""docstring for A"""
+		def __init__(self, arg):
+			self.__arg = arg - в цьому класі це імя буде виглядадти як '_A__arg'
+
+	class B(B):
+		"""docstring for B"""
+		def __init__(self, arg):
+			super().__init__() - після цього визветься клас А і після цього в екземплярі В зяветься змінна __arg
+			self.__arg = 1 - і тоді ми отримуємо дві змінні (таким чимон захищаємо від перевизначення) {'_B__arg':1,'_A_arg':}
+			
+
+	MRO - methor resolution order (вирішення множинного наслідування)
 
 
+					.object
+
+				.A 		.B  	.C
+			 .D	  .E  	
+				.I     .F
+					.G
+						.H
+
+	Для побудови MRO почали застосовувати алгоритм С3
+	він забеспечує дві важливі властивості локального старшинства. Якщо в класі H є піддерево ієрархій то в предку піддерево буде зберігатись
+	Як він працює - цей алгоритм він так саме працює за правилом зліва на право але для кожного класу будується лінерізація 
+	для класу А це буде (A,object)
+	для класу B це буде (B,object)
+	для класу C це буде (C,object)
+	I(D,E)
+	E(A)
+	і це будується в список
+	далі ми починаємо рухатись зіва в гору(якщо клас знаходиться в )
+	H.mro()
+	H -> G > I -> D -> E -> A -> F -> B -> C -> object
+	якщо B(A), C(B,A) то тоді C.mro() -> OK!
+	якщо B(A), C(A,B) - C.mro() -> runtime error
+	-----------------super()---------------------------:
+		B(A)
+	super().__init__ - ок
+
+	class Shape:
+		"""docstring for Shape"""
+		def __init__(self, pos):
+			self.pos = pos
+
+	class Circle(Shape):
+			"""docstring for Circle"""
+			def __init__(self, pos,r):
+				super().__init__(pos)
+				self.r = r;
 
 
+	class ColoredShape(Shape):
+			"""docstring for ClassName"""
+			def __init__(self, pos,col):
+				self.col = col
+				self.pos = pos
+
+	class StripedShape(Shape):
+		"""docstring for StripedShape"""
+		def __init__(self, pos,width):
+			self.pos = pos
+			self.width = width
+	
+	class ColoredCircle(Circle,ColoredShape):
+		"""docstring for ColoredCircle"""
+		def __init__(self,partos,r,color):
+			super().__init__(pos,r)
+
+	class A(object):
+		"""docstring for A"""
+		a = 1
+		def __init__(self, arg):
+			super(A, self).__init__()
+			self.arg = arg
 	
 
+	-------ДВА ЦІКАВИХ ДЕКОРАТОРА---------:
+		class A:
+			@staticmethod
+			def hello():
+
+		A.f()
+
+		class A:
+			@classmethod
+			def g(cls): -> отримуємо сам клас
+
+	-----------------UML------------------:
+		залежність - 
+		спадкування - 
+		агрегація - 
 
 
 
 
+	class Persone(object):
+		"""docstring for Persone"""
+		def __init__(self, name, age):
+			self.name = name
+			self.age = age
 
+		def __repr__(self):
+			return "Persone('{}'.{})".format(self.name,self.age)
+
+		def __str__(self):
+			return "{},{}".format(self.name,self.age)
+		"""
+		[0.1]
+		"""
+		def __eq__(self,other):
+			if isinstance(other,Persone):
+				return self.name == other.name and self.age == other.age
+			return False
+		"""
+		[0.2]
+		"""
+		def __lt__(self,other):
+		return self.name < other.name and self.age < other.age
+
+		def __hash__(self):
+			return hash(self.name)
+
+
+	p1 = Persone('Artem',19)
+	p2 = Persone('Bill',30)
+	p1 == p2 ---> False 	[0.1]
+
+	l = [Persone('Bill',30),Persone('Bill',30)]
+
+	l.index(Persone('Bill',30)) -> також буде працювати оскільки порівнюємо на індекс
+	l.sort() -> не працювати
+
+	реалізуємо компаратор [0.2]
+
+
+	class Number:
+		"""docstring for Number"""
+		def __init__(self, value):
+			self.value = value
+
+		def __repr__(self):
+			return 'Number(%d)'%self.value
+
+		def __add__(self,other): ----> Number(2) + 1 -> OK
+			if isinstance(other,Number):
+				return Number(self.value+other.value)
+			return Number(self.value + other)
+
+		def __radd__(self,other):   ---> 1 + Number(2) -> OK
+			return self.__add__(self,other)
+
+		"""iterator"""
+		Martin Fauler "Проектрования больше нет" знайти цю статтю
+						Proecting is gone
+
+
+	два способи реалізувати інтерфейсь
+	Агрегація, Наслідування
+
+	ми можемо успадкуватись від класу list і отримати додатковий функціонал
+	ми список поміщаємо в середині класу тому в середині класу має бути реалізований інтерфейс для функцій
+
+	Спадкування краще не юзати бо перетягуємо весь інтерфейс
+	----------СПАДКУВАННЯ-----------:
+		class  MyList(list):
+
+			def sum(self): 				l = MyList([1,2,3])
+				s = 0
+				for i in self:
+					s+=i
+				return s
+				
+			
+	------------АГРЕГАЦІЯ-----------:
+		class MyList(object):
+			"""docstring for MyList"""
+			def __init__(self,l=[]):		-> l = MyList([1,2,3])
+				self._l = list(l)
+
+			def __len__(self):				-> len(l)
+				return len(self._l)
+
+			def __str__(self):				-> str(l)
+				return str(self._l)
+
+			def __repr__(self):				-> repr(l)
+				return repr(self._l)
+
+			def add(self,value):			->
+				self._l.append(value)
+
+			def __setitem__(self,index,value):
+				self._l[index] = value
+
+			def __getitem__(self,index):
+				if isinstance(index,slice):
+					return index(self,_l)
+				elif isinstance(index,tuple):
+					return[self._l[x] for x in index]
+				elif index == Elipsis:
+					return self._l.copy()
+				else:
+					return self._l[index]
+
+
+
+				{
+					l[1:5] --> slice(1,5,0)
+					l[1:5,3:4,5]
+					l[...]
+					for i in MyList([1,2,3]):
+						print i
+						{1,2,3}
+				}
+
+			def __contains__(self,value):
+				return value in self._l
+
+			--------new iterator-------------:
+				def __iter__(self):
+					return iter(self._l)	-> easy way =)
+			--------pichalni iterator-------------:
+				def __iter__(self):
+					self._i = 0
+					return self
+
+				def __next__(self):
+					self.i+=1
+					if(self.i > len(self._l)):
+						raise StopIteration
+					return self._l[self.i-1]
+						{
+							for i in l:
+								for j in l:
+									print(i,j) 			-> НІХЕРА НЕ ПРАЦЮЄ
+						}
+			--------good iterator-------------:
+				class MyListIterator:
+					def __init__(self,l,i=0):
+						self.l = l
+						self.i = i
+					def __iter__(self):	
+						return MyListIterator(self.l,self.i)
+
+					def __next__(self):
+						self.i+=1
+						if self.i > len(self.l):
+							raise StopIteration
+						return self.l[self.i - 1]
+
+
+				...
+				MyList
+				...
+				def __iter__(self):
+					return MyListIterator(self)
+
+			--------original iterator-------------:
+				def f():
+					for i in range(5):
+						yield i
+
+				g = f()
+				for i in g:
+					print(i)
+
+				def __iter__(self):
+					for i in self._l:
+						yield i
+
+				l = [x*x for x in range(5)]
+				---------------
+				l1 = (x*x for x in range(5)) дозволяе не обчислювати все відразу
+					lazy calculation
+				
+				for i in l1:
+					if i>5:
+						break
+					print (i)
+
+				def f(x,y):
+					if x>5:
+						return 2*y
+					return 0
+
+				f(0,g(...))
+	в стандартній бібліотеці пайтон є модуль itertools 
+	там є функції і різні способи використання yield
+
+	------------------------05/13/16--------------------------:
+		
+
+	
 
 
 
